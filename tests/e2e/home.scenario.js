6c33793cf98b178b368851c683b5cdf9ad115d63
Mert Yazicioglu <mertyazicioglu@nokta.com>
2016-09-19 00:39:00 +0300

    Fixed the E2E test case

--- tests/e2e/home.scenario.js
+++ tests/e2e/home.scenario.js
@@ -6,11 +6,11 @@ var chaiAsPromised = require('chai-as-promised');
 
 chai.use(chaiAsPromised);
 
-beforeEach(function () {
-  browser.get('/');
-});
-
 describe('Application', function () {
+  beforeEach(function () {
+    browser.get('/');
+  });
+
   it('home page should greet', function (done) {
     var heading = element(by.css('h1'));
     chai.expect(heading.getText()).to.eventually.equal('Hello World!');
